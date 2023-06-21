827723fe4b14605e15d227f18158dd26daa5610d
Prakash Timilsina <prakashtimilsina84@gmail.com>
2023-06-21 00:02:26 -0500

    useReducer with ts

---
+++ contextApiANDuseReducerWithTypeScript/src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+    <div className="container">
+      <h1>Hello Prakash</h1>
+      
+    </div>
+    </>
+  );
+}
+
+export default App;
